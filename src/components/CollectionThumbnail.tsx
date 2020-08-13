42b785e2f4f42d2c94d49285aae25e28870bf3b3
Alex Motoc <alexmotoc97@gmail.com>
2020-08-13 11:55:25 +0100

    Fixed bug with collection title appearing too early and overlay not working on title hover

--- src/components/CollectionThumbnail.tsx
+++ src/components/CollectionThumbnail.tsx
@@ -28,16 +28,20 @@ export const CollectionThumbnail: React.FunctionComponent<CollectionProps> = (pr
         <GridListTile 
             className="image-tile"
             onClick={() => handleThumbnailClick(`${match.path}/${props.title.toLowerCase().split(' ').join('-')}`)} 
-            onMouseEnter={showCollectionTitle} 
-            onMouseLeave={hideCollectionTitle} 
             key={props.title} 
             component={Grid} 
             item 
             md={props.thumbnailSize || 4} 
             xs={12} 
             cols={props.thumbnailSize ? props.thumbnailSize / 4 : 1}>
-                <img className="collection-thumbnail" src={props.thumbnail} alt={props.title} />
-                {showTitle && <span className="collection-thumbnail-title">{props.title}</span>}
+                <img 
+                    onMouseEnter={showCollectionTitle} 
+                    onMouseLeave={hideCollectionTitle} 
+                    style={{opacity: showTitle ? 0.6 : 1}} 
+                    className="collection-thumbnail" 
+                    src={props.thumbnail} 
+                    alt={props.title} />
+                {showTitle && <span onMouseEnter={showCollectionTitle} className="collection-thumbnail-title">{props.title}</span>}
         </GridListTile>
     );
 }
