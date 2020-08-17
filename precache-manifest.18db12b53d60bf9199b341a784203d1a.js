0e78dfe242a3380c80f8c5a13597b4b5a400c879
Alex Motoc <alexmotoc97@gmail.com>
2020-08-17 21:21:12 +0100

    Updates

---
+++ precache-manifest.18db12b53d60bf9199b341a784203d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128eec9866c12e348364903829b6db32",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1821fcaeda614393bbd7",
+    "url": "/portfolio/static/css/main.ddc3fc36.chunk.css"
+  },
+  {
+    "revision": "1ef5210541b182c32890",
+    "url": "/portfolio/static/js/2.c9e5d2fe.chunk.js"
+  },
+  {
+    "revision": "e4279675a9c60179159c82343fcf3639",
+    "url": "/portfolio/static/js/2.c9e5d2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1821fcaeda614393bbd7",
+    "url": "/portfolio/static/js/main.e1195643.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
